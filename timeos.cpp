2bfd50e30c1441cbd8764bf02fe02ba5fcf6ef48
Tadas <vtadas25@gmail.com>
2018-05-05 09:30:58 +0300

    Skeleton

--- timeos.cpp
+++ timeos.cpp
@@ -2,53 +2,17 @@
  *  @file
  *  @copyright defined in eos/LICENSE.txt
  */
-#include <timeos.hpp>
-#include <timeos.gen.hpp>
+#include "timeos.hpp"
 
-namespace CONTRACT_NAME {
-
-  void store_timestamp(uint64_t scope, uint64_t table, const timestamp& ts) {
-    bytes date = eosio::raw::pack(ts.date);
-    ::store_str(scope, table, (char*)ts.data.get_data(), ts.data.get_size(), (char*)date.data, date.len);
-  }
-
-  void create_timestamp(const timestamp_order& order) {
-    eosio::require_auth(order.owner);
-    assert(!is_timestamped(order), "Data is already timestamped");
-    timestamp ts = timestamp(order);
-    store_timestamp(order.owner, N(timestamp), ts);
-  }
-
-  void remove_timestamp(const timestamp_order& order) {
-    eosio::require_auth(order.owner);
-    auto success = remove_str(order.owner, N(timestamp), (char*)order.data.get_data(), order.data.get_size());
-    assert(success == 1, "Timestamp with this data does not exist");
-  }
-}
-
-using namespace CONTRACT_NAME;
 /**
  *  The init() and apply() methods must have C calling convention so that the blockchain can lookup and
  *  call these methods.
  */
 extern "C" {
 
-  /**
-  *  This method is called once when the contract is published or updated.
-  */
-  void init()  {
-  }
-
-  /// The apply method implements the dispatch of events to this contract
-  void apply( uint64_t code, uint64_t action ) {
-    if (code == CONTRACT_NAME_UINT64) {
-      if (action == N(createts)) {
-        create_timestamp(eosio::current_message<timestamp_order>());
-      }
-      else if (action == N(removets)) {
-        //FIXME: Should removing timestamp be allowed?
-        remove_timestamp(eosio::current_message<timestamp_order>());
-      }
+    /// The apply method implements the dispatch of events to this contract
+    void apply( uint64_t receiver, uint64_t code, uint64_t action ) {
+       eosio::print( "Hello World: ", eosio::name{code}, "->", eosio::name{action}, "\n" );
     }
-  }
+
 } // extern "C"
--- timeos.hpp
+++ timeos.hpp
@@ -1,56 +1,7 @@
-#pragma once
-
-#include <eoslib/eos.hpp>
-#include <eoslib/string.hpp>
-#include <eoslib/db.hpp>
-
-#define CONTRACT_NAME timeos
-#define CONTRACT_NAME_UINT64 N(timeos)
-
-namespace CONTRACT_NAME {
-
-  //@abi action createts removets
-  struct timestamp_order {
-    eosio::string data;
-    account_name  owner;
-  };
-
-  //@abi table str
-  struct timestamp {
-    eosio::string data;
-    time          date;
-
-    timestamp() : date(0) {}
-    timestamp(const eosio::string& data, time date) : data(data), date(date) {}
-    //Create timestamp with time from previous block
-    timestamp(const timestamp_order& order) : data(order.data) {
-      date = now();
-    }
-  };
-
-  inline int32_t load_timestamp(account_name owner, const eosio::string& data, time& time) {
-    return load_str(owner, CONTRACT_NAME_UINT64, N(timestamp), (char*)data.get_data(),
-             data.get_size(), (char*)&time, sizeof (time));
-  }
-
-  // Returns default timestamp value if data is not stored in the db
-  timestamp get_timestamp(account_name owner, const eosio::string& data) {
-    time t;
-    auto r = load_timestamp(owner, data, t);
-    return (r > -1) ? timestamp(data, t) : timestamp();
-  }
-
-  bool is_timestamped(account_name owner, const eosio::string& data) {
-    time t;
-    auto r = load_timestamp(owner, data, t);
-    return r > -1;
-  }
-
-  inline bool is_timestamped(const timestamp_order& order) {
-    return is_timestamped(order.owner, order.data);
-  }
-
-}
-
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+#include <eosiolib/eosio.hpp>
 
 
